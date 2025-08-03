141a188a7e9e27a076d031cc84f78b0618955531
Flor Rossi <mfloro.91@gmail.com>
2025-08-03 15:00:12 -0300

    vite env

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
 });
 
 
-console.log("Backend base URL:", import.meta.env.BASE_URL);
+console.log("Backend base URL:", import.meta.env.VITE_BASE_URL);
 
 export default axiosInstance;
