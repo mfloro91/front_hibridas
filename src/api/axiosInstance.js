2edada1810d1d1a101f9a14c1ba8d8ea70e50cf5
Flor Rossi <mfloro.91@gmail.com>
2025-08-03 14:30:34 -0300

    debug conexion front / back

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true,
+  
 });
+
+console.log("Backend base URL:", process.env.REACT_APP_BASE_URL);
 
 export default axiosInstance;
